7226b54eb0baf42714460cd940264319c47c27b6
Neha Wasnik <nehawasnik95@outlook.com>
2024-10-27 13:42:35 +0530

    Thala For A Reason

---
+++ starter113/ThalaForAReason.java
@@ -0,0 +1,16 @@
+package starter113;
+
+import java.util.Scanner;
+
+public class ThalaForAReason {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+        if (number == 7){
+            System.out.println("THALA");
+        } else {
+            System.out.println("SADGE");
+        }
+    }
+}
