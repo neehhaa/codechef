9d5ddcf44dfabbfc6186a672dd2e85255006e2b2
Neha Wasnik <nehawasnik95@outlook.com>
2024-10-27 12:31:15 +0530

    Codechef Round

---
+++ starter115/CodechefRound.java
@@ -0,0 +1,16 @@
+package starter115;
+
+import java.util.Scanner;
+
+public class CodechefRound {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int day = input.nextInt();
+        if (day == 4){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
