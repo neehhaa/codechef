6fce225eb7a26e61b5c8dc0ff1677708da8a1245
Neha Wasnik <nehawasnik95@outlook.com>
2024-11-02 17:15:18 +0530

    Algomaniac Finals

---
+++ starter123/AlgomaniacFinals.java
@@ -0,0 +1,16 @@
+package starter123;
+
+import java.util.Scanner;
+
+public class AlgomaniacFinals {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int date = input.nextInt();
+        if (date == 17){
+            System.out.println("YAY");
+        }else {
+            System.out.println("No");
+        }
+    }
+}
